5402f12ac81587eff86b905738111941797335e8
Jean <jean@pop-os.localdomain>
2021-04-29 23:46:37 -0300

    Iniciado a construção da funcionalidade de tipo pedreiro

---
+++ src/main/java/br/com/modelo/entidade/pedreiro/TipoPedreiro.java
@@ -0,0 +1,14 @@
+package br.com.modelo.entidade.pedreiro;
+
+import br.com.modelo.entidade.Entidade;
+
+/**
+ * Classe para representar a entidade tipo_pedreiro
+ *
+ * A entidade tipo_pedreiro armazena os tipos de pedreiro
+ *
+ * @author Jean Varela
+ * data    29/04/2021
+ */
+public class TipoPedreiro extends Entidade {
+}
