b8c74233d8c08b7f04e0e83c161a7151dfe55429
Jean Varela <jean.varela1@gmail.com>
2021-08-11 23:12:37 -0300

    Melhorada a configuração do banco de dados no banco de dados e criada a miigration para criar o usuário

--- src/main/resources/db/migration/V001__criacao.sql
+++
@@ -1,8 +0,0 @@
-create table usuario (
-  id bigint not null,
-  nome  varchar(100) not null,
-  email varchar(50) not null,
-  senha varchar(30) not null,
-
-  primary key (id)
-)
---
+++ src/main/resources/db/migration/V001__criacao_usuario.sql
@@ -0,0 +1,32 @@
+CREATE SEQUENCE public.hibernate_sequence
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.hibernate_sequence
+    OWNER TO postgres;
+
+create table tipo_usuario (
+  id bigint not null,
+  nome  varchar(50) not null,
+
+  primary key (id)
+);
+
+insert into tipo_usuario (id,nome) values (1,'Pedreiro');
+insert into tipo_usuario (id,nome) values (2,'Cliente');
+
+create table usuario (
+  id bigint not null,
+  nome  varchar(100) not null,
+  email varchar(50) not null,
+  senha varchar(30) not null,
+  tipo_usuario_id INTEGER NOT NULL,
+
+
+  primary key (id)
+);
+
+ALTER TABLE usuario  ADD FOREIGN KEY (tipo_usuario_id) REFERENCES tipo_usuario(id);
