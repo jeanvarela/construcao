468a7ec467b8ab8d1ba6610333dc22ba5fd879cb
Jean <jean.varela1@gmail.com>
2022-08-27 00:15:08 -0300

    Iniciada a reestruturação do projeto

--- src/main/resources/db/migration/V001__criacao_usuario.sql
+++ src/main/resources/db/migration/V001__criacao_usuario.sql
@@ -1,19 +1,9 @@
-CREATE SEQUENCE public.hibernate_sequence
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
-
-ALTER SEQUENCE public.hibernate_sequence
-    OWNER TO postgres;
-
 create table usuario (
-  id bigint not null,
+  id bigint not null auto_increment,
   nome  varchar(100) not null,
   email varchar(50) not null,
   senha varchar(30) not null,
   cpf   varchar(12) not null,
 
   primary key (id)
-);
+)engine=InnoDB default charset=utf8;
