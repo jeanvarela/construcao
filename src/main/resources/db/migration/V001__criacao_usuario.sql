601e0c3c88dadea0c9cbe9f7967e43dbd4d19472
Jean Varela <jean.varela1@gmail.com>
2023-05-01 01:13:25 -0300

    Criado as primeira migrações do banco

--- src/main/resources/db/migration/V001__criacao_usuario.sql
+++
@@ -1,9 +0,0 @@
-create table usuario (
-  id bigint not null auto_increment,
-  nome  varchar(100) not null,
-  email varchar(50) not null,
-  senha varchar(30) not null,
-  cpf   varchar(12) not null,
-
-  primary key (id)
-)engine=InnoDB default charset=utf8;
